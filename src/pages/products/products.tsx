d9a6768bf7a5aca51a89969f9546e92d47cf5341
Ulan <aukdevgm@gmail.com>
2025-03-26 16:04:27 +0600

    add products filter

--- src/pages/products/products.tsx
+++ src/pages/products/products.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { PorductCard } from "../../components/product-card/product-card";
 import { PorductCardSkeleton } from "../../components/product-card/product-card.skeleton";
 import { getProducts, getProductsStatus } from "../../store/products/selectors";
@@ -7,11 +8,23 @@ import styles from "./products.module.scss";
 export const Products = () => {
   const products = getProducts();
   const status = getProductsStatus();
+  const [filter, setFilter] = useState("All");
 
   if (status === "failed") return <p>Failed to load products.</p>;
 
+  const filteredProducts =
+    filter === "All" ? products : products.filter((product) => product.liked);
+
   return (
     <>
+      <select
+        className={styles.filter}
+        onChange={(e) => setFilter(e.target.value)}
+      >
+        <option value="All">All</option>
+        <option value="Liked">Liked</option>
+      </select>
+
       <ul className={styles.list}>
         {status === "loading"
           ? [1, 2, 3, 4, 5, 6].map((product) => (
@@ -19,7 +32,7 @@ export const Products = () => {
                 <PorductCardSkeleton />
               </li>
             ))
-          : products.map((product) => (
+          : filteredProducts.map((product) => (
               <li key={product.id}>
                 <PorductCard key={product.id} product={product} />
               </li>
